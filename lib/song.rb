d3762e3ab0a4a4ad2a5d19af6a5f82d8a7688f23
Deborah Barcenas <deborah.barcenas@gmail.com>
2019-05-03 17:21:27 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -6,10 +6,10 @@ class Song
     @@all
   end
 
-  def initialize(name)
-    @name = name
-    @@all << self
-  end
+  # def initialize(name)
+  #   @name = name
+  #   @@all << self
+  # end
 
   def save
     self.class.all << self
