b8719ca59cdc7e244051ff67cba4c22a85dc7db1
Deborah Barcenas <deborah.barcenas@gmail.com>
2019-05-03 19:24:49 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -9,7 +9,7 @@ class Song
   def save
     self.class.all << self
   end
-
+  
   def self.create
     new_song = self.new
     new_song.save
@@ -29,8 +29,8 @@ class Song
     new_song
   end
 
-  def self.find_by_name
-
+  def self.find_by_name(string)
+    self.all.find {|song| song.string == string}
   end
 
 end
