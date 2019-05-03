b06245974e28f6d03a55c11a7180cf623a50a71d
Deborah Barcenas <deborah.barcenas@gmail.com>
2019-05-03 19:32:27 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -33,4 +33,11 @@ class Song
     self.all.find {|song| song.name == string}
   end
 
+  def Song.find_or_create_by_name(string)
+    # return a matching song instance with that name 
+    # or 
+    # create a new song with the name and return the song instance.
+    self.find_by_name(string) || self.create_by_name(string)
+  end
+
 end
